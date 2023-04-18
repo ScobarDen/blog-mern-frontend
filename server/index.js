17b3709e1183bc94186a98e2a1b1317247286593
Denis Guschin <gdv-ps@yandex.ru>
2023-04-18 13:01:58 +0300

    теперь бэк должен возвращать фронт

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ import cors from "cors";
 import config from "config";
 import routes from "./routes/index.js";
 import chalk from "chalk";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const PORT = config.get("port");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -18,6 +22,16 @@ process.env.NODE_ENV === "production" &&
   console.log(chalk.blue("Production environment"));
 process.env.NODE_ENV === "development" &&
   console.log(chalk.blue("Development environment"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
+
+  const indexPath = path.join(__dirname, "client", "index.html");
+
+  app.get("*", (req, res) => {
+    res.sendFile(indexPath);
+  });
+}
 
 async function start() {
   try {
